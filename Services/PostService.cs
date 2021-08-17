367ddd3488bb4252e6e19cf1e94b0e6613246957
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-08-17 19:12:35 -0400

    paging added. Post service added. Updated links and User restrictions

---
+++ Services/PostService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TitanBlog.Data;
+using TitanBlog.Models;
+
+namespace TitanBlog.Services
+{
+    public class PostService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Post>> GetIndexPosts()
+        {
+            return await _context.Posts.Where(p => p.IsReady).OrderByDescending(p => p.Created).ToListAsync();
+        }
+    }
+}
